d9635d74d05e9e906a31633e58767d3c33b020c1
hayleymcq <hayleymcq@gmail.com>
2023-12-13 15:55:28 +1100

    added API

--- java.js
+++ java.js
@@ -21,18 +21,30 @@ let todaysDate = document.querySelector(".todays-date");
 
 todaysDate.innerHTML = `${day} ${hour}:${minute}`;
 
+function updateTodayTemp(response) {
+  let todayTemp = document.querySelector(".main_temp");
+  let temperature = response.data.temperature.current;
+  todayTemp.innerHTML = Math.round(temperature);
+   let h1 = document.querySelector("h1");
+    h1.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+let apiKey = "cdo60b4f4t3a9a806fbe5b8a3494aa91";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateTodayTemp)
+}
 
 function citySearchResult(event) {
   event.preventDefault();
-   let h1 = document.querySelector("h1");
    let searchFormInput = document.querySelector("#search-form-input");
-  h1.innerHTML = searchFormInput.value;
- 
+  searchCity(searchFormInput.value);
 }
 
 let changeCity = document.querySelector(".search-form");
 changeCity.addEventListener("submit", citySearchResult);
 
+searchCity("Sydney");
 
 
 
