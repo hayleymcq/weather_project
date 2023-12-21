1f155e898432c561e1b34086c4c116a8978c81b6
hayleymcq <hayleymcq@gmail.com>
2023-12-21 15:48:31 +1100

    created working forecast

--- java.js
+++ java.js
@@ -15,6 +15,8 @@ function updateTodayTemp(response) {
   time.innerHTML = formatDate(date);
   let icon = document.querySelector("#icon");
   icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -46,6 +48,43 @@ function citySearchResult(event) {
   event.preventDefault();
   let searchFormInput = document.querySelector("#search-form-input");
   searchCity(searchFormInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let forecastDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return forecastDays[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "cdo60b4f4t3a9a806fbe5b8a3494aa91";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+<div class = "forecast-container">
+<div class="forecast-day">${formatDay(day.time)}</div>
+            <img class="forecast-icon" src = "${day.condition.icon_url}" />
+            <div class="forecast-temp">
+              <span class="min-temp">${Math.round(
+                day.temperature.minimum
+              )}°</span>
+              <span class="max-temp">${Math.round(
+                day.temperature.maximum
+              )}°</span>
+            </div>
+            </div>`;
+    }
+  });
+  let forecast = document.querySelector(".forecast");
+  forecast.innerHTML = forecastHtml;
 }
 
 let changeCity = document.querySelector(".search-form");
